338431ae82b1e91821722c09a6b55f29a677343a
Jeff Goeders <jeff.goeders@gmail.com>
2022-02-21 01:14:46 +0000

    Update for 2022
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
 # DAC 2022 Design Contest
 
-# Note:
-These files are still under development.  They should be stable by the end of February.
-
-For full contest details, please see the [2021 DAC System Design Contest](https://dac-sdc-2021.groups.et.byu.net/doku.php) page.
+For full contest details, please see the [2022 DAC System Design Contest](https://dac-sdc-2021.groups.et.byu.net/doku.php) page.
 
 For general questions regarding this contest, please use the Piazza page: piazza.com/dac_2018/winterterm12021/dacsdc2021
 
 ## Setup PYNQ on your Ultra96v2 board
 
-  * Download the Ultra96v2 board image from http://www.pynq.io/board.html
-  * Follow the instructions to image the SD card at https://pynq.readthedocs.io/en/latest/getting_started/pynq_image.html.  
-  * Follow the instructions to setup and connect to the board at https://ultra96-pynq.readthedocs.io/en/latest/getting_started.html.
+  * Download the PYNQ 2.7 Ultra96v2 board image from <http://www.pynq.io/board.html>
+  * Follow the instructions to image the SD card at <https://pynq.readthedocs.io/en/latest/getting_started/pynq_image.html>.  
+  * Follow the instructions to setup and connect to the board at <https://ultra96-pynq.readthedocs.io/en/latest/getting_started.html>.
 
 ## Usage
 The get started, users have to run the following command on the Ultra96 board:
 
 ```shell
 cd /home/xilinx/jupyter_notebooks
-sudo git clone https://github.com/jgoeders/dac_sdc_2021.git
+git clone https://github.com/jgoeders/dac_sdc_2022.git
 ```
 Remember the user name and password are both `xilinx` for super user.
 
-After the above step is completed successfully, you will see a folder `dac_sdc_2021` under your 
+After the above step is completed successfully, you will see a folder `dac_sdc_2022` under your 
 jupyter notebook dashboard.  Open the `sample_team/dac_sdc.ipynb` notebook for directions on where to begin.
 
 ## Folder Structure
 
-1. sample_team: This folder contains files for a sample team.  This includes a <teamname>.bit and <teamname>.tcl file that defines the hardware, and a `.ipynb` jupyter notebook, and a `hw` folder that is used to create a Vivado project.
+1. sample_team: This folder contains files for a sample team.  This includes a <teamname>.bit and <teamname>.tcl file that defines the hardware, and a `.ipynb` jupyter notebook, and a `hw` folder that is used to create a Vivado project.  You should create a new folder for your team, where you will keep all of your files.
 
-2. images: All the test images are stored in this folder.
+2. images: All the test images are stored in this folder.  Replace the example images in this directory with the full training set.
 
 3. result: The results contain the output xml produced when execution is complete, and contains the runtime, energy usage, and predicted location of each object in each image.
 
--- common/dac_sdc.py
+++ common/dac_sdc.py
@@ -6,33 +6,29 @@ import pynq
 import cv2
 import sys
 
-DAC_CONTEST = pathlib.Path('/home/xilinx/jupyter_notebooks/dac_sdc_2022/')
-IMG_DIR = DAC_CONTEST / 'images'
-RESULT_DIR = DAC_CONTEST / 'result'
-    
-BATCH_SIZE = 500
+DAC_CONTEST = pathlib.Path("/home/xilinx/jupyter_notebooks/dac_sdc_2022/")
+IMG_DIR = DAC_CONTEST / "images"
+RESULT_DIR = DAC_CONTEST / "result"
 
-class Batch:
-    pass
+BATCH_SIZE = 1000
 
-    
 # Return a batch of image dir  when `send` is called
 class Team:
     def __init__(self, team_name):
         self._result_path = RESULT_DIR / team_name
-        self.team_dir = DAC_CONTEST / team_name      
+        self.team_dir = DAC_CONTEST / team_name
 
         folder_list = [self.team_dir, self._result_path]
         for folder in folder_list:
             if not folder.is_dir():
                 folder.mkdir()
-        
+
         self.img_list = self.get_image_paths()
         self.current_batch_idx = 0
 
     def get_image_paths(self):
-        names_temp = [f for f in IMG_DIR.iterdir() if f.suffix == '.jpg']
-        names_temp.sort(key= lambda x:int(x.stem))
+        names_temp = [f for f in IMG_DIR.iterdir() if f.suffix == ".jpg"]
+        names_temp.sort(key=lambda x: int(x.stem))
         return names_temp
 
     # Returns list of images paths for next batch of images
@@ -44,7 +40,7 @@ class Team:
 
     def get_bitstream_path(self):
         return str(self.team_dir / "dac_sdc.bit")
-    
+
     def reset_batch_count(self):
         self.current_batch_idx = 0
 
@@ -55,7 +51,7 @@ class Team:
 
         rgb_imgs = []
         for image_path in image_paths:
-            bgr_img = cv2.imread(str(image_path))    
+            bgr_img = cv2.imread(str(image_path))
             rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
             rgb_imgs.append((image_path, rgb_img))
 
@@ -67,34 +63,50 @@ class Team:
         self.__result_rectangles = []
 
         rails = pynq.get_rails()
-        rails_to_monitor=["1V2", "PSDDR", "INT", "PSINT_LP", "PSINT_FP", "PSPLL"]
+        rails_to_monitor = ["1V2", "PSDDR", "INT", "PSINT_LP", "PSINT_FP", "PSPLL"]
 
-        while(True):
+        while True:
             # Load images to memory
             rgb_imgs = self.load_images_to_memory()
             if not rgb_imgs:
                 break
 
             if debug:
-                print("Starting batch of", len(rgb_imgs), "images")
+                print("Batch", self.current_batch_idx, "starting.", len(rgb_imgs), "images.")
 
             # Run user callback, recording runtime and power usage
             start = time.time()
             recorder = pynq.DataRecorder(*[rails[r].power for r in rails_to_monitor])
-            with recorder.record(0.05):      
+            with recorder.record(0.05):
                 object_locations = callback(rgb_imgs)
             end = time.time()
 
             if len(object_locations) != len(rgb_imgs):
-                raise ValueError(str(len(rgb_imgs)) + " images provided, but only " + str(len(object_locations)) + " object locations returned.")
+                raise ValueError(
+                    str(len(rgb_imgs))
+                    + " images provided, but only "
+                    + str(len(object_locations))
+                    + " object locations returned."
+                )
             self.__result_rectangles.extend(object_locations)
 
             runtime = end - start
-            energy = sum([recorder.frame[r + "_power"].mean() for r in rails_to_monitor]) * runtime   
+            energy = (
+                sum([recorder.frame[r + "_power"].mean() for r in rails_to_monitor])
+                * runtime
+            )
 
-            if (debug):
-                print("Done batch.  Runtime =", runtime, "seconds. Energy =", energy, "J.")
-            
+            if debug:
+                print(
+                    "Batch",
+                    self.current_batch_idx,
+                    "done. Runtime =",
+                    runtime,
+                    "seconds. Energy =",
+                    energy,
+                    "J.",
+                )
+
             self.__total_time += runtime
             self.__total_energy += energy
 
@@ -102,87 +114,67 @@ class Team:
             del rgb_imgs[:]
             del rgb_imgs
 
-        print("Done all batches.")
+        print(
+            "Done all batches. Total runtime =",
+            self.__total_time,
+            "seconds. Total energy =",
+            self.__total_energy,
+            "J.",
+        )
 
-        print("Savings results to XML")
+        print("Savings results to XML...")
         self.save_results_xml()
         print("XML results written successfully.")
 
-        # Start timer and energy recorder.  If this fails, then you may not have configured
-        # your system to measure power usage.  Follow the steps provided at
-        # https://github.com/jgoeders/dac_sdc_2020/blob/master/support/measure_power/README.md
-    
-        # aux - no
-        # with recorder.record(0.05):        
-        #     while True:
-        
-        #         for img in rgb_imgs:
-        #             in_buffer[:] = img    
-        #             dma_transfer()
-                    
-        # timer stop after batch processing is complete
-            
-        # Energy measurements    
-
-        # total_time = t
-        # total_energy = energy
-        # print("Total time:", total_time, "seconds")
-        # print("Total energy:", total_energy, "J")
-            
     def save_results_xml(self):
         if len(self.__result_rectangles) != len(self.img_list):
             raise ValueError("Result length not equal to number of images.")
 
         doc = xml.dom.minidom.Document()
-        root = doc.createElement('results')
+        root = doc.createElement("results")
 
-        perf_e = doc.createElement('performance')
-        
+        perf_e = doc.createElement("performance")
+
         # Runtime
-        runtime_e = doc.createElement('runtime')
+        runtime_e = doc.createElement("runtime")
         runtime_e.appendChild(doc.createTextNode(str(self.__total_time)))
         perf_e.appendChild(runtime_e)
         root.appendChild(runtime_e)
 
         # Energy
-        energy_e = doc.createElement('energy')
+        energy_e = doc.createElement("energy")
         energy_e.appendChild(doc.createTextNode(str(self.__total_energy)))
         perf_e.appendChild(energy_e)
         root.appendChild(energy_e)
-
 
         for i, rectangle in enumerate(self.__result_rectangles):
             image_e = root.appendChild(doc.createElement("image"))
 
             doc.appendChild(root)
-            name_e = doc.createElement('filename')
+            name_e = doc.createElement("filename")
             name_t = doc.createTextNode(self.img_list[i].name)
             name_e.appendChild(name_t)
             image_e.appendChild(name_e)
 
-            size_e = doc.createElement('size')
-            node_width = doc.createElement('width')
+            size_e = doc.createElement("size")
+            node_width = doc.createElement("width")
             node_width.appendChild(doc.createTextNode("640"))
-            node_length = doc.createElement('length')
+            node_length = doc.createElement("length")
             node_length.appendChild(doc.createTextNode("360"))
             size_e.appendChild(node_width)
             size_e.appendChild(node_length)
             image_e.appendChild(size_e)
 
-            object_node = doc.createElement('object')
-            node_bnd_box = doc.createElement('bndbox')
-            node_bnd_box_xmin = doc.createElement('xmin')
-            node_bnd_box_xmin.appendChild(
-                doc.createTextNode(str(rectangle[0])))
-            node_bnd_box_xmax = doc.createElement('xmax')
-            node_bnd_box_xmax.appendChild(
-                doc.createTextNode(str(rectangle[1])))
-            node_bnd_box_ymin = doc.createElement('ymin')
-            node_bnd_box_ymin.appendChild(
-                doc.createTextNode(str(rectangle[2])))
-            node_bnd_box_ymax = doc.createElement('ymax')
-            node_bnd_box_ymax.appendChild(
-                doc.createTextNode(str(rectangle[3])))
+            object_node = doc.createElement("object")
+            node_bnd_box = doc.createElement("bndbox")
+            node_bnd_box_xmin = doc.createElement("xmin")
+            node_bnd_box_xmin.appendChild(doc.createTextNode(str(rectangle[0])))
+            node_bnd_box_xmax = doc.createElement("xmax")
+            node_bnd_box_xmax.appendChild(doc.createTextNode(str(rectangle[1])))
+            node_bnd_box_ymin = doc.createElement("ymin")
+            node_bnd_box_ymin.appendChild(doc.createTextNode(str(rectangle[2])))
+            node_bnd_box_ymax = doc.createElement("ymax")
+            node_bnd_box_ymax.appendChild(doc.createTextNode(str(rectangle[3])))
             node_bnd_box.appendChild(node_bnd_box_xmin)
             node_bnd_box.appendChild(node_bnd_box_xmax)
             node_bnd_box.appendChild(node_bnd_box_ymin)
@@ -192,5 +184,5 @@ class Team:
             image_e.appendChild(object_node)
 
         file_name = self._result_path / "results.xml"
-        with open(file_name, 'w') as fp:
-            doc.writexml(fp, indent='\t', addindent='\t', newl='\n', encoding="utf-8")
+        with open(file_name, "w") as fp:
+            doc.writexml(fp, indent="\t", addindent="\t", newl="\n", encoding="utf-8")
